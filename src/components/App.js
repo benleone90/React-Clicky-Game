a0c582f2700f67c3a822f60ce86adda3da54bcfe
Ben Leone <benleone90@users.noreply.github.com>
2019-05-09 21:35:55 -0400

    added page components and updated favicon

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,13 @@
 import React, { Component } from "react";
+import Banner from "./Banner";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Banner />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/images.js
@@ -0,0 +1,29 @@
+import arya from "./images/arya.jpg";
+import bran from "./images/bran.jpg";
+import cersi from "./images/cersi.jpg";
+import hound from "./images/hound.jpg";
+import jamie from "./images/jamie.jpg";
+import joffery from "./images/joffery.jpg";
+import jon from "./images/jon.jpg";
+import ned from "./images/ned.jpg";
+import sam from "./images/sam.jpg";
+import theon from "./images/theon.jpg";
+import tyrion from "./images/tyrion.jpg";
+import varys from "./images/varys.jpg";
+
+const images = [
+  arya,
+  bran,
+  cersi,
+  hound,
+  jamie,
+  joffery,
+  jon,
+  ned,
+  sam,
+  theon,
+  tyrion,
+  varys
+];
+
+export default images;
